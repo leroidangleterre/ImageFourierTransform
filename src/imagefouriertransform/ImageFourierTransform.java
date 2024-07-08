5ce3ca0149370d259d372f47ecd1495029089c85
Arthur Manoha <arthur.manoha@gmail.com>
2024-07-08 03:31:50 +0200

    added 6 image view

--- src/imagefouriertransform/ImageFourierTransform.java
+++ src/imagefouriertransform/ImageFourierTransform.java
@@ -8,14 +8,16 @@ public class ImageFourierTransform {
 
     public static void main(String[] args) {
 
-        String folder = "C:\\Users\\arthu\\Documents\\Programmation\\Java\\ImageFourierTransform\\src\\imagefouriertransform\\";
-//        String filename = "a.bmp";
-        String filename = "a_small.bmp";
-        String completeFilename = folder + filename;
-        MyImage myImage = new MyImage(completeFilename);
-//        boolean makeSymmetrical = false;
-//        MyImage myImage = new MyImage(5, 5, makeSymmetrical);
-        ImageWindow viewer = new ImageWindow(myImage);
+//        String folder = "C:\\Users\\arthu\\Documents\\Programmation\\Java\\ImageFourierTransform\\src\\imagefouriertransform\\";
+////        String filename = "a.bmp";
+////        String filename = "a_small.bmp";
+//        String filename = "test.bmp";
+//        String completeFilename = folder + filename;
+//        MyImage myImage = new MyImage(completeFilename);
+        boolean makeSymmetrical = false;
+        MyImage imageA = new MyImage(20, 20, makeSymmetrical);
+        MyImage imageB = new MyImage(20, 20, makeSymmetrical);
+        ImageWindow viewer = new ImageWindow(imageA, imageB);
 
     }
 
